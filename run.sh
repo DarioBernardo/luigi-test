5b049370d9814b141d02c2bcc68a0263e89adb08
darber <dario.bernardo@unibet.com>
2016-08-02 15:20:02 +0100

    conda environment script fix

--- run.sh
+++ run.sh
@@ -4,19 +4,20 @@
 # /opt/anaconda3/bin/activate
 
 
+source_var="activate"
 
 if [ $# -eq 0 ]
   then
     echo "No arguments supplied. Using the default 'activate' (If exists!)."
-    $source_var = activate
   else
-  echo "Using activate at:"
-  echo $1
-  $source_var = $1
+    echo "Using activate at:"
+    echo $1
+    source_var=$1
 fi
 
 source $source_var py352
 
+echo "Using python version:"
 python --version
 
 # python src/Update_temperature.py
